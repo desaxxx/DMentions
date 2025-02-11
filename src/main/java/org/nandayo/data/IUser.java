50c269a86fe654f164928eaba7ca10e44d5d0cfd
Desa <76732650+desaxxx@users.noreply.github.com>
2025-02-12 00:17:53 +0300

    Delete src/main/java/org/nandayo/data directory

--- src/main/java/org/nandayo/data/IUser.java
+++
@@ -1,9 +0,0 @@
-package org.nandayo.data;
-
-import org.bukkit.entity.Player;
-
-public interface IUser {
-
-    boolean getMentionMode(Player player);
-    void setMentionMode(Player player, boolean mode);
-}
